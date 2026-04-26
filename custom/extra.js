ba7c8f53c4e92f48b89d21d91c85757340318175
Robert A. Petit III <robert@french5s.com>
2026-04-25 23:57:31 -0600

    Snapshot v3.1.0 (migrated from static/)

---
+++ custom/extra.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.zoom').magnificPopup({
+        type:'image',
+        removalDelay: 600,
+        mainClass: 'mfp-fade',
+        closeOnContentClick: true,
+        showCloseBtn: false
+    });
+});
